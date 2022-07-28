5bc1a2e3ba16a5559bead3b9f15c77c1d8ee979b
windstep <icq431@yandex.ru>
2022-07-28 07:36:07 +0300

    feat: added "place" to change event

--- src/VueGoogleAutocomplete.vue
+++ src/VueGoogleAutocomplete.vue
@@ -24,7 +24,7 @@
         administrative_area_level_1: 'short_name',
         administrative_area_level_2: 'long_name',
         country: 'long_name',
-        postal_code: 'short_name'
+        postal_code: 'short_name',
     };
 
     const CITIES_TYPE = ['locality', 'administrative_area_level_3'];
@@ -344,6 +344,7 @@
 
                 returnData['latitude'] = place.geometry.location.lat();
                 returnData['longitude'] = place.geometry.location.lng();
+                returnData['place'] = place;
                 return returnData
             },
 
